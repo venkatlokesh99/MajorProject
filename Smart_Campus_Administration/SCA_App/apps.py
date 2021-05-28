9f8e3c60f3a3337daf4a5cae90de4d93c4c27e79
Vejendla Venkat Lokesh <venkatlokesh999@gmail.com>
2021-05-29 00:17:12 +0530

    Added recaptcha to login

---
+++ Smart_Campus_Administration/SCA_App/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ScaAppConfig(AppConfig):
+    name = 'SCA_App'
---
+++ Smart_Campus_Administration/SCA_App/views.py
@@ -0,0 +1,48 @@
+import json
+import requests
+
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.urls import reverse
+
+from SCA_App.ValidateEmail import EmailBackEnd
+
+
+# Create your views here.
+
+def ShowLoginPage(request):
+    return render(request,"login_page.html")
+
+def doLogin(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        captcha_token=request.POST.get("g-recaptcha-response")
+        cap_url="https://www.google.com/recaptcha/api/siteverify"
+        cap_secret="6Lefh5IaAAAAAD9xSEliN31acHrssDaMgA9Uf7l1"
+        cap_data={"secret":cap_secret,"response":captcha_token}
+        cap_server_response=requests.post(url=cap_url,data=cap_data)
+        cap_json=json.loads(cap_server_response.text)
+
+        if cap_json['success']==False:
+            messages.error(request,"Invalid Captcha Try Again")
+            return HttpResponseRedirect("/")
+            
+        user=EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
+        if user!=None:
+            login(request,user)
+            if user.user_type=="1":
+                return HttpResponseRedirect('/admin_home')
+            elif user.user_type=="2":
+                return HttpResponseRedirect(reverse("staff_home"))
+            else:
+                return HttpResponseRedirect(reverse("student_home"))
+        else:
+            messages.error(request,"Invalid Login Details")
+            return HttpResponseRedirect("/")
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect("/")
